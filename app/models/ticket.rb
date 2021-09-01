925387ba35ed72c68b89d459b9d9a46e3b175946
Erik Ecoologic <erikecoologic@gmail.com>
2021-09-02 08:21:45 +1000

    Cleanup

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,16 +4,33 @@ module Models
   class Ticket < Models::Base
     def decorated_record
       record.merge(
-        organization_name: ModelDatabase.new(:organizations)
-                                        .records_by_id[record[:organization_id]]
-                                        .to_h[:name],
-        submitter_name: ModelDatabase.new(:users)
-                                     .records_by_id[record[:submitter_id]]
-                                     .to_h[:name],
-        assignee_name: ModelDatabase.new(:users)
-                                    .records_by_id[record[:assignee_id]]
-                                    .to_h[:name]
+        organization_name: organization_name,
+        submitter_name: submitter_name,
+        assignee_name: assignee_name
       )
+    end
+
+    private
+
+    def organization_name
+      ModelDatabase
+        .new(:organizations)
+        .records_by_id[record[:organization_id]]
+        .to_h[:name]
+    end
+
+    def submitter_name
+      ModelDatabase
+        .new(:users)
+        .records_by_id[record[:submitter_id]]
+        .to_h[:name]
+    end
+
+    def assignee_name
+      ModelDatabase
+        .new(:users)
+        .records_by_id[record[:assignee_id]]
+        .to_h[:name]
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,33 @@ module Models
   class User < Models::Base
     def decorated_record
       record.merge(
-        organization_name: ModelDatabase.new(:organizations)
-                                        .records_by_id[record[:organization_id]]
-                                        .to_h[:name],
-        assigned_ticket_subjects: ModelDatabase.new(:tickets)
-                                               .relation_records(:assignee, record[:_id])
-                                               .map { |r| r[:subject] },
-        submitter_ticket_subjects: ModelDatabase.new(:tickets)
-                                                .relation_records(:submitter, record[:_id])
-                                                .map { |r| r[:subject] }
+        organization_name: organization_name,
+        assigned_ticket_subjects: assigned_ticket_subjects,
+        submitter_ticket_subjects: submitter_ticket_subjects
       )
+    end
+
+    private
+
+    def organization_name
+      ModelDatabase
+        .new(:organizations)
+        .records_by_id[record[:organization_id]]
+        .to_h[:name]
+    end
+
+    def assigned_ticket_subjects
+      ModelDatabase
+        .new(:tickets)
+        .relation_records(:assignee, record[:_id])
+        .map { |r| r[:subject] }
+    end
+
+    def submitter_ticket_subjects
+      ModelDatabase
+        .new(:tickets)
+        .relation_records(:submitter, record[:_id])
+        .map { |r| r[:subject] }
     end
   end
 end
